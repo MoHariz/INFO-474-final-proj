2d8099322637295c97c40f121212280c4e70fd70
Liuyang Fu <zzl19970122@gmail.com>
2019-06-08 05:06:15 -0700

    asd

--- learn-idyll/components/bar-chart.js
+++ learn-idyll/components/bar-chart.js
@@ -3,7 +3,6 @@ const D3Component = require("idyll-d3-component");
 const d3 = require("d3");
 
 class BarChart extends D3Component {
-<<<<<<< HEAD
   initialize(node, props) {
     // node is a <div> container,
     console.log(props.data);
@@ -26,18 +25,9 @@ class BarChart extends D3Component {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // for sorted barchart. comment it if not want sorted data
-    // props.data.sort(function(a, b) {
-    //     return a.total_NA_sales - b.total_NA_sales;
-    // });
-
-    svg
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", 0 - margin.top / 2)
-      .attr("text-anchor", "middle")
-      .style("font-size", "16px")
-      .style("text-decoration", "underline")
-      .text("Value vs Date Graph");
+    props.data.sort(function(a, b) {
+      return a.total_NA_sales - b.total_NA_sales;
+    });
 
     var x = d3.scaleLinear().range([0, width]);
 
@@ -98,78 +88,6 @@ class BarChart extends D3Component {
   update(props) {
     // ...
   }
-=======
-    initialize(node, props) {
-        // node is a <div> container,
-        console.log(props.data)
-        var margin = {
-            top: 20,
-            right: 20,
-            bottom: 30,
-            left: 100
-          };
-
-        var width = 500 - margin.left - margin.right;
-        var height = 500 - margin.top - margin.bottom;
-    
-        var svg = d3.select(node).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        // for sorted barchart. comment it if not want sorted data
-        props.data.sort(function(a, b) {
-            return a.total_NA_sales - b.total_NA_sales;
-        });
-    
-        var x = d3.scaleLinear()
-              .range([0, width]);
-    
-        var y = d3.scaleBand()
-            .range([height, 0]);
-    
-        var xAxis = d3.axisBottom(x)
-            .ticks(10);
-    
-        var yAxis = d3.axisLeft(y);
-
-        x.domain([0, d3.max(props.data, function(d) { return d.total_NA_sales; })]);
-
-        y.domain(props.data.map(function(d) { return d.Genre; }))
-          .paddingInner(0.1)
-          .paddingOuter(0.5);
-  
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-          .append("text")
-            .attr("class", "label")
-            .attr("transform", "translate(" + width + ",0)")
-            .attr("y", -5)
-            .style("text-anchor", "end")
-            .text("Total North America Sales (in millions)");
-  
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
-  
-        svg.selectAll(".bar")
-            .data(props.data)
-          .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", 0)
-            .attr("height", y.bandwidth())
-            .attr("y", function(d) { return y(d.Genre); })
-            .attr("width", function(d) { return x(d.total_NA_sales); });
-
-    }
-
-    update(props) {
-        // ...
-    }
->>>>>>> 62573e0fd1c6e36b415367fad3a383915f26875b
 }
 
 module.exports = BarChart;
--- learn-idyll/components/what-if.js
+++ learn-idyll/components/what-if.js
@@ -2,31 +2,23 @@ const React = require("react");
 const d3 = require("d3");
 
 class WhatIf extends React.PureComponent {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = { genre: this.props.genre };
-
-  //     this.updateGenre = this.updateGenre.bind(this);
-  //   }
-  updateStats(e) {
-    debugger;
-    this.props.updateProps({
-      med: e.med,
-      max: e.max
-    });
-  }
-
   render() {
     let data = [];
     for (let i = 0; i < this.props.data.length; i++) {
       data.push(this.props.data[i].Global_Sales);
     }
     let sortedData = data.sort((a, b) => a - b);
-    debugger;
     let median = d3.median(sortedData);
     let maxAbsolute = d3.max(sortedData);
-    this.updateStats({ med: median, max: maxAbsolute });
-    return null;
+
+    return (
+      <div>
+        game, the median prediction sales will be{" "}
+        <span className="sales-number">{median}</span> million copies, with the
+        largest sale of <span className="sales-number">{maxAbsolute}</span>{" "}
+        million copies.
+      </div>
+    );
   }
 }
 module.exports = WhatIf;
