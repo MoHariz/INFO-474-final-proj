e281142aec34a750695a580b324a39fda5c0c6db
Liuyang Fu <zzl19970122@gmail.com>
2019-06-08 02:38:17 -0700

    a

--- learn-idyll/components/what-if.js
+++ learn-idyll/components/what-if.js
@@ -1,41 +1,32 @@
 const React = require("react");
+const d3 = require("d3");
 
 class WhatIf extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { genre: this.props.genre };
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = { genre: this.props.genre };
 
-    this.updateGenre = this.updateGenre.bind(this);
-    this.uodatePlatform = this.updatePlatform.bind(this);
-  }
-  updateGenre(e) {
+  //     this.updateGenre = this.updateGenre.bind(this);
+  //   }
+  updateStats(e) {
+    debugger;
     this.props.updateProps({
-      genre: e.target.value
+      med: e.med,
+      max: e.max
     });
   }
 
-  updatePlatform(e) {
-    this.props.updateProps({
-      platform: e.target.value
-    });
-  }
   render() {
-    return (
-      <div className="what-if">
-        <span>If I am a developer who want to make a </span>
-        <select name="genres" onChange={this.updateGenre}>
-          <option value="platform">Platform</option>
-          <option value="shooter">Shooter</option>
-        </select>
-        <span> game at </span>
-        <select name="platform" onChange={this.updateGenre}>
-          <option value="XBOX">XBOX</option>
-          <option value="PS">PlayStation</option>
-          <option value="PC">PC</option>
-        </select>
-        <span> platform, there's 75% of chances that I will sale:</span>
-      </div>
-    );
+    let data = [];
+    for (let i = 0; i < this.props.data.length; i++) {
+      data.push(this.props.data[i].Global_Sales);
+    }
+    let sortedData = data.sort((a, b) => a - b);
+    debugger;
+    let median = d3.median(sortedData);
+    let maxAbsolute = d3.max(sortedData);
+    this.updateStats({ med: median, max: maxAbsolute });
+    return null;
   }
 }
 module.exports = WhatIf;
